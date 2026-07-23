48cd2d036b4e13b7b6718a0f62c2cde3c69b10df
Alex <113833988+skif0013@users.noreply.github.com>
2026-07-23 16:54:55 +0200

    Add dark theme styling for Swagger UI

--- src/Kamsoft.ContentParser.Api/Program.cs
+++ src/Kamsoft.ContentParser.Api/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +9,13 @@ builder.Services.AddControllers()
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+app.UseStaticFiles();
 
 app.MapControllers();
 
 app.Run();
+
